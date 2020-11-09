f429c8c29762e2b4535159215c2c5657d9e55da7
PHI25\peter <peter.silva.escola@gmail.com>
2020-11-09 14:28:10 +0000

    commit in 09-11-2020

---
+++ code101/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>School - PHP</title>
+</head>
+
+<body>
+	<h1>School - PHP</h1>
+	<p><?php
+		echo("Hoje é " . date("Y/m/d"). "<br>");
+	    ?>
+    </p>
+	<img src="images/php.png" width="375" height="360" alt=""/>
+    <br>
+
+    <h1>Part II – Learning the Language</h1>
+    <ul>
+        <li><a href="#">PHP Language Basics</a></li>
+        <li><a href="#">Decisions and Loops</a></li>
+        <li><a href="#">Strings</a></li>
+        <li><a href="#">Arrays</a></li>
+        <li><a href="#">Functions</a></li>
+        <li><a href="#">Objects</a></li>
+    </ul>
+	
+</body>
+</html>
